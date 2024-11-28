7b3c8a3b16f075c9fd96e0d3aee6d6bfe4513241
dlalstlr12 <as2984348@naver.com>
2024-11-28 22:40:27 +0900

    feat : BCrypt를 사용하여 비밀번호 암호화

--- src/main/java/com/reservation/board/service/UserService.java
+++ src/main/java/com/reservation/board/service/UserService.java
@@ -2,31 +2,34 @@ package com.reservation.board.service;
 
 import com.reservation.board.model.User;
 import com.reservation.board.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class UserService {
   private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
-  public UserService(UserRepository userRepository) {
+  public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
     this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
   }
 
   public User registerUser(User user) {
     if (userRepository.findByUsername(user.getUsername()).isPresent()) {
       throw new IllegalStateException("Username already exists!");
     }
+    user.setPassword(passwordEncoder.encode(user.getPassword())); // 비밀번호 암호화
     return userRepository.save(user);
   }
 
   public User loginUser(String username, String password) {
-    Optional<User> user = userRepository.findByUsername(username);
-    if (user.isPresent() && user.get().getPassword().equals(password)) {
-      return user.get();
-    } else {
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new IllegalStateException("Invalid username or password!"));
+
+    if (!passwordEncoder.matches(password, user.getPassword())) {
       throw new IllegalStateException("Invalid username or password!");
     }
+    return user;
   }
 }
